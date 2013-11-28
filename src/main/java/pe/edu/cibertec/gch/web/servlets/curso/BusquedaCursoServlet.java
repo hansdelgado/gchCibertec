31c3b633bae6e6a748f8fb0b7a94381b08fb276f
mariocrusz <mariocrusz@hotmail.com>
2013-11-27 21:32:07 -0500

    Validacion, Filtro Cursos
    Ver. 1.0

--- src/main/java/pe/edu/cibertec/gch/web/servlets/curso/BusquedaCursoServlet.java
+++ src/main/java/pe/edu/cibertec/gch/web/servlets/curso/BusquedaCursoServlet.java
@@ -1,9 +1,6 @@
-/*
- * To change this template, choose Tools | Templates
- * and open the template in the editor.
- */
 package pe.edu.cibertec.gch.web.servlets.curso;
 
+import pe.edu.cibertec.gch.web.servlets.curso.*;
 import java.io.IOException;
 import java.util.List;
 import javax.servlet.ServletException;
@@ -13,26 +10,29 @@ import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 import pe.edu.cibertec.gch.logica.GestorCurso;
 import pe.edu.cibertec.gch.modelo.Curso;
+import pe.edu.cibertec.gch.modelo.TipoBusqueda;
 import pe.edu.cibertec.gch.web.servlets.GchServletUtils;
 
 /**
- *
- * @author JAVA-ADV-LM
+ * Servlet para buscar profesores.
  */
-@WebServlet(name = "ListadoCursoServlet", urlPatterns = {"/listarCursos"})
-public class ListadoCursoServlet extends HttpServlet {
+@WebServlet(name = "BusquedaCursoServlet", urlPatterns = {"/buscarCurso"})
+public class BusquedaCursoServlet extends HttpServlet {
+
     private GestorCurso gestorCurso = new GestorCurso();
-    
 
     @Override
     protected void doGet(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
+        String textoCodigo = req.getParameter("codigo"),
+                textoNombre = req.getParameter("nombre"),
+                textoTipoBusqueda = req.getParameter("tipoBusqueda");;
+        // segun el tipo de codigo obtenemos el tipo de busqueda
+        TipoBusqueda tipoBusqueda = TipoBusqueda.obtenerPorCodigo(Integer.parseInt(textoTipoBusqueda));
         // trae los profesores en la fuente de datos
-        List<Curso> cursos = gestorCurso.listarTodos();
+        List<Curso> cursos = gestorCurso.listarSegunCurso(textoCodigo,textoNombre, tipoBusqueda);
         // almacena resultado en el request
         req.setAttribute("cursos", cursos);
         // pinta los datos en el listado
-        
         GchServletUtils.reenviarAModulo("curso", req, resp);
     }
-    
 }
