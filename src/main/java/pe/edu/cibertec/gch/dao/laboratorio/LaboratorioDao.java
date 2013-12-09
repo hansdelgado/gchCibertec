bfcf81bf6d9ae4ce618cd29df36c10390555595a
yklino <yklino@gmail.com>
2013-12-08 19:52:40 -0500

    laboratorioDao

--- src/main/java/pe/edu/cibertec/gch/dao/laboratorio/LaboratorioDao.java
+++ src/main/java/pe/edu/cibertec/gch/dao/laboratorio/LaboratorioDao.java
@@ -2,6 +2,7 @@ package pe.edu.cibertec.gch.dao.laboratorio;
 
 import java.util.List;
 import pe.edu.cibertec.gch.dao.BaseDao;
+import pe.edu.cibertec.gch.modelo.EstadoLaboratorio;
 import pe.edu.cibertec.gch.modelo.Laboratorio;
 import pe.edu.cibertec.gch.modelo.TipoBusqueda;
 
@@ -11,7 +12,7 @@ import pe.edu.cibertec.gch.modelo.TipoBusqueda;
  */
 public interface LaboratorioDao extends BaseDao<Laboratorio> {
     void eliminarPorCodigo(final String codigo);
-    void actualizar( final String codigo, String nombre, String descripcion,  String local);
+    void actualizar( final String codigo, String nombre, String descripcion,  String local,String pabellon,String salon, Integer capacidad,EstadoLaboratorio estado);
     Laboratorio consultarPorCodigo(final String codigo);
     List<Laboratorio> listarSegun(final String nombre, final String local, TipoBusqueda tipoBusqueda);
 }
